899f5491956c4b8bc3964da0d83fdf92613de077
saidurrahmanmisket <mdsaidurrahmanmisket@gmail.com>
2025-12-30 16:00:34 +0600

    feat: Dynamically generate product names and corresponding image URLs in the product factory.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,29 +16,35 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $products = [
+            'Miniket Rice (5kg)' => 'rice',
+            'Rupchanda Soybean Oil (5L)' => 'oil',
+            'ACI Pure Salt (1kg)' => 'salt',
+            'Radhuni Turmeric Powder' => 'turmeric',
+            'Pran Potato Crackers' => 'chips',
+            'Ispahani Mirzapore Tea' => 'tea',
+            'Bashundhara Tissue Box' => 'tissue',
+            'Lux Soap' => 'soap',
+            'Sunsilk Shampoo' => 'shampoo',
+            'Lifebuoy Hand Wash' => 'handwash',
+            'Dano Milk Powder' => 'milk',
+            'Maggi Noodles (Pack of 8)' => 'noodles',
+            'Aarong Dairy Ghee' => 'ghee',
+            'Deshi Lentils (Moshur Dal)' => 'lentils',
+            'Teer Sugar (1kg)' => 'sugar',
+        ];
+
+        $name = $this->faker->randomElement(array_keys($products));
+        $keyword = $products[$name];
+
         return [
             'user_id' => 1, // Will be overridden
             'category_id' => 1, // Will be overridden
-            'name' => $this->faker->randomElement([
-                'Miniket Rice (5kg)',
-                'Rupchanda Soybean Oil (5L)',
-                'ACI Pure Salt (1kg)',
-                'Radhuni Turmeric Powder',
-                'Pran Potato Crackers',
-                'Ispahani Mirzapore Tea',
-                'Bashundhara Tissue Box',
-                'Lux Soap',
-                'Sunsilk Shampoo',
-                'Lifebuoy Hand Wash',
-                'Dano Milk Powder',
-                'Maggi Noodles (Pack of 8)',
-                'Aarong Dairy Ghee',
-                'Deshi Lentils (Moshur Dal)',
-                'Teer Sugar (1kg)'
-            ]),
+            'name' => $name,
             'price' => $this->faker->randomFloat(2, 50, 2000), // Adjusted price range for BD context
             'unit' => $this->faker->randomElement(['kg', 'pcs', 'L', 'pack', 'box']),
-            'img_url' => 'https://placehold.co/600x400/EEE/31343C',
+            'img_url' => "https://loremflickr.com/600/400/{$keyword}?lock=" . $this->faker->randomNumber(5),
+            // Added lock param to ensure consistent but different images per call if needed, or just random
         ];
     }
 }
